91f85e2a0712f2b8fb0d3d88635dab4a4a3b6a74
Kaoruko <yykaoruko@gmail.com>
2020-03-18 15:26:23 +0900

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'import/prefer-default-export': 'off',
+    'no-alert': 'off',
   },
   overrides: [
     {
